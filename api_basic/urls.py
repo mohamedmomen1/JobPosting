36cf3f92cd58ad599e2fd8dc12523080c40ce412
mohamedmomen1 <68752016+mohamedmomen1@users.noreply.github.com>
2021-09-02 00:12:08 +0300

    Update urls.py

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from .views import UserDetailsView, ChangeUserAPIView, UsersView, Job_postingDetailsView, ChangeJob_postingAPIView, \
-    Job_postingView
+    Job_postingView, HRRsView, HRRDetailsView, ChangeHRRAPIView, CompanyView, ChangeCompanyAPIView, CompanyDetailsView, \
+    Job_postingDetailsView1
 
 # urlpatterns = [
 #   path('ViewSet/', include(router.urls)),
@@ -23,10 +24,19 @@ urlpatterns = [
     path('user/<int:pk>', UserDetailsView.as_view()),
     path('user/', UsersView.as_view()),
 
-    path('user/<int:pk>', ChangeUserAPIView.as_view()),
+    path('user/<int:pk>/change', ChangeUserAPIView.as_view()),
+
+    path('HRR/<int:pk>', HRRDetailsView.as_view()),
+    path('HRR/', HRRsView.as_view()),
+    path('HRR/<int:pk>/change', ChangeHRRAPIView.as_view()),
+
+    path('Company/<int:pk>', CompanyDetailsView.as_view()),
+    path('Company/', CompanyView.as_view()),
+    path('Company/<int:pk>/change', ChangeCompanyAPIView.as_view()),
 
     path('Job/<int:pk>', Job_postingDetailsView.as_view()),
     path('Job/', Job_postingView.as_view()),
     path('Job/<int:pk>/change', ChangeJob_postingAPIView.as_view()),
+    path('Job1/<int:company>', Job_postingDetailsView1.as_view())
 
 ]
