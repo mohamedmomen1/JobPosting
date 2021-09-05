eb9f2c42f29ba11000ee4ca8b5ddafe5e735d24a
mohamedmomen1 <68752016+mohamedmomen1@users.noreply.github.com>
2021-09-05 12:57:37 +0300

    Update admin.py

--- api_basic/admin.py
+++ api_basic/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 # from .models import hRRs
 from .models import EndUser
-# from .models import company
+from .models import Company
+from .models import HRRUser
 # from .models import Eu_emails
 # from .models import Employment_history
-# from .models import Eu_employer
+from .models import EndUserEmployer
 from .models import JobPosting
+from .models import Department
 
 # from .models import Manager_job_posting
 # from .models import Application
@@ -13,12 +15,13 @@ from .models import JobPosting
 # from .models import InternshipJobPosting
 
 # Register your models here.
-# admin.site.register(hRRs)
+admin.site.register(HRRUser)
 admin.site.register(EndUser)
-# admin.site.register(company)
+admin.site.register(Company)
+admin.site.register(Department)
 # admin.site.register(Eu_emails)
 # admin.site.register(Employment_history)
-# admin.site.register(Eu_employer)
+admin.site.register(EndUserEmployer)
 admin.site.register(JobPosting)
 # admin.site.register(Manager_job_posting)
 # admin.site.register(Application)
