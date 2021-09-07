3fa8b5e3a9ea0c5a2e9b335037ede87cc226be6f
mohamedmomen1 <68752016+mohamedmomen1@users.noreply.github.com>
2021-09-07 14:12:11 +0300

    Update urls.py

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import UserDetailsView, ChangeUserAPIView, UsersView, Job_postingDetailsView, ChangeJob_postingAPIView, \
     Job_postingView, HRRsView, HRRDetailsView, ChangeHRRAPIView, CompanyView, ChangeCompanyAPIView, CompanyDetailsView, \
-    Job_postings_for_companyView, remove  # addEndUserEmployer
+    Job_postings_for_companyView, remove, create_hrrView, addEndUserEmployer,addApplication,Enduserlist
 
 # urlpatterns = [
 #   path('ViewSet/', include(router.urls)),
@@ -34,7 +34,13 @@ urlpatterns = [
     path('Company/', CompanyView.as_view()),
     path('Company/<int:pk>/change', ChangeCompanyAPIView.as_view()),
 
+    path('addEndUserEmployer/',addEndUserEmployer.as_view()),
+    path('Enduserlist',Enduserlist.as_view()),
     path('removeEmployer/<int:username>', remove.as_view()),
+
+    path('createHRR', create_hrrView.as_view()),
+    path('addapplication',addApplication.as_view()),
+
     path('Job/<int:pk>', Job_postingDetailsView.as_view()),
     path('Job/', Job_postingView.as_view()),
     path('Job/<int:pk>/change', ChangeJob_postingAPIView.as_view()),
