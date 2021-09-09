63f3e087136bd6945945e685ceb8e2b6894c2391
mohamedmomen1 <68752016+mohamedmomen1@users.noreply.github.com>
2021-09-09 03:18:24 +0300

    Update models.py

--- api_basic/models.py
+++ api_basic/models.py
@@ -21,9 +21,6 @@ class HRRUser(models.Model):
 
     end_user = models.ForeignKey(EndUser, on_delete=models.PROTECT)
 
-    def get_absolute_url(self):
-        return f"/client/HRR/{self.id}"
-
     def __str__(self):
         return self.username
 
@@ -97,8 +94,8 @@ class Department(models.Model):
     company = models.ForeignKey(Company, on_delete=models.PROTECT)
     job_posting = models.ForeignKey(JobPosting, on_delete=models.PROTECT)
 
-    def get_absolute_url(self):
-        return f"/client/Department/{self.id}"
+    # def get_absolute_url(self):
+    #     return f"/client/Department/{self.id}"
 
     def __str__(self):
         return self.name
